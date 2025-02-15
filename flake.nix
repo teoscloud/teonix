dd546e2877dcd3c05d0c9bac8f78640dd212172e
Teodor <199249708+teoscloud@users.noreply.github.com>
2025-02-15 02:40:15 +0100

    modularized monitor for hyprland different hosts

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit username; };
+            home-manager.extraSpecialArgs = { inherit username hostname; };
             home-manager.users.${username} = import ./home/${username}.nix;
           }
         ];
