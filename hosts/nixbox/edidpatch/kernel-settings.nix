8f83b96eead32e84120130f79b6b9656386aff27
Teodor <199249708+teoscloud@users.noreply.github.com>
2025-07-19 16:31:39 +0200

    fixed monitor outputs

--- hosts/nixbox/edidpatch/kernel-settings.nix
+++ hosts/nixbox/edidpatch/kernel-settings.nix
@@ -4,8 +4,8 @@
   boot = {
 
     kernelParams = [
-      "drm.edid_firmware=DP-1:edid/g9.bin"
-      "video=DP-1:e"
+      #"drm.edid_firmware=DP-1:edid/g9.bin"
+      # "video=DP-1:e"
       #"ipv6.disable=1"
     ];
 
